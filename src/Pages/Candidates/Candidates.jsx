d5dbc3954ef937a69b1e00279426668a8b9ddf17
BIT Student <classroom@bgit.rs>
2022-04-12 12:59:48 +0200

    singlecandidatepage-70%done

--- src/Pages/Candidates/Candidates.jsx
+++ src/Pages/Candidates/Candidates.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { ctx } from '../../Components/ProviderComp/ProviderComp';
 import { useContext } from 'react';
 import Header from '../../Components/Header/Header';
+import "./Candidates.scss"
+import { Link } from 'react-router-dom';
 export const Candidates = () => {
   const value = useContext(ctx);
   return (
-    <>
+    <div className='candidates-guestpage-container-master'>
       <Header></Header>
-      <div>
+      <div className="candidates-guestpage-container">
         {value.candidatesData.map((e) => {
-          return <p key={e.id}>{e.name}</p>;
+          console.log(e)
+          return <Link to={`candidates/${e.id}`}><div className='candidate-guestpage-wrapperPrima'><div className='candidate-guestpage-wrapper' key={e.id}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ic_person_48px.svg/240px-Ic_person_48px.svg.png" />
+            <h2 className='candidates-guestpage-ime'>{e.name}</h2>
+            <p>{e.birthday}</p>
+            <p>{e.education}</p>
+            <p>{e.email}</p>
+          </div></div></Link>
         })}
       </div>
-    </>
+    </div>
   );
 };
 
