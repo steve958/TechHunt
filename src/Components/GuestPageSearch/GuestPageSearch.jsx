21174afaab0bcb0b894786c3b308269314cd92e5
steve958 <stevenex958@gmail.com>
2022-04-13 15:56:27 +0200

    candpage-finished

--- src/Components/GuestPageSearch/GuestPageSearch.jsx
+++ src/Components/GuestPageSearch/GuestPageSearch.jsx
@@ -1,45 +1,44 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { ctx } from '../ProviderComp/ProviderComp';
 import { useContext } from 'react';
-import "./GuestPageSearch.scss"
-import { Link } from "react-router-dom";
+import './GuestPageSearch.scss';
+import { Link } from 'react-router-dom';
 
-const GuestPageSearch = () => {
+// const GuestPageSearch = (props) => {
+//   const [searchInput, setsearchInput] = useState('');
 
-    const [searchInput, setsearchInput] = useState("")
-    const [cand, setCand] = useState([])
-    const value = useContext(ctx);
+//   const value = useContext(ctx);
 
-    const handleChange = (inputValue) => {
-        setsearchInput(inputValue)
-        setCand((value.candidatesData.filter((e) => {
-            console.log(searchInput)
-            console.log(e.name.split(" ").includes(searchInput))
+//   ;
 
-            return e.name.includes(searchInput) || e.name.toLowerCase().includes(searchInput)
+//   return (
+//     <>
+//       <input
+//         id='guestpageSearch'
+//         onChange={(e) => {
+//           handleChange(e.target.value);
+//         }}
+//         type='text'
+//         name='search'
+//         placeholder='search'
+//       />
+//       {cand.map((e) => {
+//         return (
+//           <Link to={`candidates/${e.id}`}>
+//             <div className='candidate-guestpage-wrapperPrima'>
+//               <div className='candidate-guestpage-wrapper' key={e.id}>
+//                 <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ic_person_48px.svg/240px-Ic_person_48px.svg.png' />
+//                 <h2 className='candidates-guestpage-ime'>{e.name}</h2>
+//                 <p>{e.birthday}</p>
+//                 <p>{e.education}</p>
+//                 <p>{e.email}</p>
+//               </div>
+//             </div>
+//           </Link>
+//         );
+//       })}
+//     </>
+//   );
+// };
 
-        })))
-
-
-    }
-
-
-    return (
-        <>
-            <input id="guestpageSearch" onChange={(e) => {
-                handleChange(e.target.value)
-            }} type="text" name="search" placeholder="search" />
-            {cand.map((e) => {
-                return <Link to={`candidates/${e.id}`}><div className='candidate-guestpage-wrapperPrima'><div className='candidate-guestpage-wrapper' key={e.id}>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ic_person_48px.svg/240px-Ic_person_48px.svg.png" />
-                    <h2 className='candidates-guestpage-ime'>{e.name}</h2>
-                    <p>{e.birthday}</p>
-                    <p>{e.education}</p>
-                    <p>{e.email}</p>
-                </div></div></Link>
-            })}
-        </>
-    );
-}
-
-export default GuestPageSearch;
+// export default GuestPageSearch;
--- src/Pages/Candidates/Candidates.jsx
+++ src/Pages/Candidates/Candidates.jsx
@@ -2,26 +2,71 @@ import React from 'react';
 import { ctx } from '../../Components/ProviderComp/ProviderComp';
 import { useContext } from 'react';
 import Header from '../../Components/Header/Header';
-import "./Candidates.scss"
+import './Candidates.scss';
 import { Link } from 'react-router-dom';
-import GuestPageSearch from '../../Components/GuestPageSearch/GuestPageSearch';
+import { useState } from 'react';
 export const Candidates = () => {
+  const [cand, setCand] = useState('');
+
+  const handleChange = (inputValue) => {
+    console.log(inputValue);
+    setCand(
+      value.candidatesData.filter((e) => {
+        return (
+          e.name.includes(inputValue) ||
+          e.name.toLowerCase().includes(inputValue)
+        );
+      })
+    );
+    // console.log(cand);
+  };
+
   const value = useContext(ctx);
   return (
     <div className='candidates-guestpage-container-master'>
       <Header></Header>
-      <GuestPageSearch></GuestPageSearch>
-      <div className="candidates-guestpage-container">
-        {value.candidatesData.map((e) => {
-          console.log(e)
-          return <Link to={`candidates/${e.id}`}><div className='candidate-guestpage-wrapperPrima'><div className='candidate-guestpage-wrapper' key={e.id}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ic_person_48px.svg/240px-Ic_person_48px.svg.png" />
-            <h2 className='candidates-guestpage-ime'>{e.name}</h2>
-            <p>{e.birthday}</p>
-            <p>{e.education}</p>
-            <p>{e.email}</p>
-          </div></div></Link>
-        })}
+      <input
+        id='guestpageSearch'
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+        type='text'
+        name='search'
+        placeholder='search'
+      />
+      <div className='candidates-guestpage-container'>
+        {!cand
+          ? value.candidatesData.map((e) => {
+              console.log(e);
+              return (
+                <Link to={`candidates/${e.id}`}>
+                  <div className='candidate-guestpage-wrapperPrima'>
+                    <div className='candidate-guestpage-wrapper' key={e.id}>
+                      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ic_person_48px.svg/240px-Ic_person_48px.svg.png' />
+                      <h2 className='candidates-guestpage-ime'>{e.name}</h2>
+                      <p>{e.birthday}</p>
+                      <p>{e.education}</p>
+                      <p>{e.email}</p>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })
+          : cand.map((e) => {
+              return (
+                <Link to={`candidates/${e.id}`}>
+                  <div className='candidate-guestpage-wrapperPrima'>
+                    <div className='candidate-guestpage-wrapper' key={e.id}>
+                      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ic_person_48px.svg/240px-Ic_person_48px.svg.png' />
+                      <h2 className='candidates-guestpage-ime'>{e.name}</h2>
+                      <p>{e.birthday}</p>
+                      <p>{e.education}</p>
+                      <p>{e.email}</p>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
       </div>
     </div>
   );
